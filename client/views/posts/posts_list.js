c08ca99b33686b5fb5123b49adb65ebd22cf6d6e
uri even ezra <uevenezra@gmail.com>
2014-04-28 06:09:30 +0000

    remove bootstarp

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,6 @@
 Template.postsList.helpers({
   postsWithRank: function() {
+    if(!this.posts) return '';
     this.posts.rewind();
     return this.posts.map(function(post, index, cursor) {
       post._rank = index;
