2ceb97188f39fc3e12e00e974e99aae906142c44
uri even ezra <uevenezra@gmail.com>
2014-04-29 05:49:53 +0000

    added Huston admin interface, added words collection,publications,subscriptions,routs , switched to bootstrap panels in postItem.html

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,13 @@ Meteor.publish('singlePost', function(id) {
   return id && Posts.find(id);
 });
 
+Meteor.publish('words', function(options) {
+  return Words.find({status : 3}, options);
+});
+
+Meteor.publish('currentWords', function() {
+  return Posts.find({status : {$all : [1,2]}});
+});
 
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
