12126be26e8768a1edfd352887c5ad1ad3ef54e0
uri even ezra <uevenezra@gmail.com>
2014-05-19 05:29:11 +0000

    added Email support (prozambot@gmail.com)

---
+++ server/smtp.js
@@ -0,0 +1,11 @@
+Meteor.startup(function () {
+  smtp = {
+    username: 'prozambot',  // eg: server@gentlenode.com
+    password: 'ProzamBot217',  // eg: 3eeP1gtizk5eziohfervU
+    server:   'smtp.gmail.com', // eg: mail.gandi.net
+    port: 25
+  }
+ 
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+  console.log(process.env.MAIL_URL);
+});
