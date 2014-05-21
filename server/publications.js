2668d03d8b035fe7747f68de4971c4ae1934a4ce
uri even ezra <uevenezra@gmail.com>
2014-05-21 12:22:58 +0000

    ROOT_URL

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,3 @@
-/*
-Meteor.publish('posts', function(options) {
-  return Posts.find({}, options);
-});
-*/
 
 Meteor.publish('singlePost', function(id) {
   return id && Posts.find(id);
--- server/smtp.js
+++ server/smtp.js
@@ -5,6 +5,11 @@ Meteor.startup(function () {
     server:   'smtp.gmail.com', // eg: mail.gandi.net
     port: 25
   }
- 
+
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+  process.env.ROOT_URL = "http://prozam.net";
+  
+  AccountsEntry.config({
+   passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
 });
